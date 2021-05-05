e0629999260de8e5c06915fb2d21618e95cc634c
Emna-Bougharriou <76558573+Emna-Bougharriou@users.noreply.github.com>
2021-05-05 22:45:02 +0100

    commir 2
    yes

--- scripts/main.js
+++ scripts/main.js
@@ -41,12 +41,15 @@ sr.reveal('.home__social-icon',{ interval: 200});
 
 /*SCROLL ABOUT*/
 sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
+sr.reveal('.about__subtitle',{delay: 400});
+sr.reveal('.about__video',{delay: 400});
+sr.reveal('.centrer',{delay: 400});
+sr.reveal('.about__text',{delay: 400});
 
 /*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skills__text',{}); 
+sr.reveal('.skills__subtitle',{});
+sr.reveal('.skills__centrer',{});
+sr.reveal('.skills__text',{});
 sr.reveal('.skills__data',{interval: 200}); 
 sr.reveal('.skills__img',{delay: 600});
 
